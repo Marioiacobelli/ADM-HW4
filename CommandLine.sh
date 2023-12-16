350c2f40fe92169e4c9c9e8f163bc12e3672fece
Marioiacobelli <131587945+Marioiacobelli@users.noreply.github.com>
2023-12-16 12:04:19 +0100

    upload main and commandline with some changes

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,43 +1,43 @@
-# Legend of the used dataset columns:
-# $3 = duration; $4 = title; $7 = movie_id; $8 = user_id
+# Legend of the used dataset columns: $3 = duration; $4 = title; $7 = movie_id; $8 = user_id
 
-csv_file="vodclickstream_uk_movies_03.csv"
+csv_file="vodclickstream_uk_movies_03.csv" 
 
-# initialize a new csv file to store the modified csv 
+# initialize a new csv file to store the modified csv dataset
 output_file="processed_dataset.csv"
-# replace all commas within the genres with blank spaces so as to preserve the structure of the csv (genres are divided by commas)
+
+# replace all commas within the genres with blank spaces so as to preserve the structure of the csv
 sed ':a;s/\("[^"]*\),\([^"]*"\)/\1 \2/;ta' "$csv_file" > "$output_file"
 
-dataset="processed_dataset.csv" # open the new created csv file 
 
 
 # QUESTION 1: What is the most-watched Netflix title?
 
-# to answer this question, I create a "movies" array using movie_ids as indexes, sum the duration of time spent on each movie, sort the results in descending order according to the second sort field and finally return the id corresponding to the first row
-film_id=$(awk -F',' '{movies[$7]+=$3} END {for (movie in movies) print movie, movies[movie]}' "$output_file" | sort -t' ' -k2,2nr | head -n 1 | awk '{print $1}')
-# I do the same to store the time (in hours) spent on that movie
-hours_spent=$(awk -F',' '{movies[$7]+=$3} END {for (movie in movies) print movie, movies[movie]}' "$output_file" | sort -t' ' -k2,2nr | head -n 1 | awk '{print $2/3600}')
-# with the most seen movie_id I can find the corrisponding title: grep is used to search in output_file a line that contains film_id, than split by , and take the value of the 4th column (title)
+# Calculate total time spent on each movie with the array 'movies', using movie_id as indexes, then find the most watched movie ID and total time
+most_watched=$(awk -F',' '{movies[$7]+=$3} END {for (movie in movies) print movie, movies[movie]}' "$output_file" | sort -t' ' -k2,2nr | head -n 1)
+# Extract movie ID and duration
+film_id=$(echo "$most_watched" | awk '{print $1}')
+hours_spent=$(echo "$most_watched" | awk '{print $2/3600}')
+# Find the movie title using the movie ID
 film_title=$(grep "$film_id" "$output_file" | head -n 1 | cut -d',' -f4)
-# print the result
+# Print the result
 echo "- Most seen movie on Netflix: $film_title with $hours_spent hours"
 
 
 
 # QUESTION 2: Report the average time between subsequent clicks on Netflix.com
 
-# to answer this question, I sum up all the clicks durations and divide by the total number of the dataset entries
-average_time=$(tail -n +2 "$dataset" | awk -F',' '{sum += $3; count++} END {print sum/count}')
+# Sum up all the clicks durations and divide by the total number of the dataset entries
+average_time=$(tail -n +2 "$output_file" | awk -F',' '{sum += $3; count++} END {print sum/count}')
 echo "- Average time between subsequent clicks: $average_time seconds"
 
 
 
 # QUESTION 3: Provide the ID of the user that has spent the most time on Netflix
 
-# to answer this question, I create a "users" array using user_ids as indexes, sum the duration of time spent by each user, sort the results in descending order according to the second sort field, and finally return the id corresponding to the first row
-user_id=$(awk -F',' '$8 != "" {users[$8]+=$3} END {for (user in users) print user, users[user]}' "$output_file" | sort -t' ' -k2,2nr | head -n 1 | awk '{print $1}')
-# I do the same to store the time (in hours) spent by the top user
-hours_spent=$(awk -F',' '$8 != "" {users[$8]+=$3} END {for (user in users) print user, users[user]}' "$output_file" | sort -t' ' -k2,2nr | head -n 1 | awk '{print $2/3600}')
-# print the result
+# Calculate total time spent by each user with the array 'users', using user_id as indexes, then find the top user ID and total time
+top_user=$(awk -F',' '$8 != "" {users[$8]+=$3} END {for (user in users) print user, users[user]}' "$output_file" | sort -t' ' -k2,2nr | head -n 1)
+# Extract user ID and total time
+user_id=$(echo "$top_user" | awk '{print $1}')
+hours_spent=$(echo "$top_user" | awk '{print $2/3600}')
+# Print the result
 echo "- User with the most total time: $user_id with $hours_spent hours"
-
